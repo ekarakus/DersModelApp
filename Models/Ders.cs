b067d10a2047b3ff2dea18e104f8ad5f6f930d4d
Ergün Karakuş <ekarakus@btofis.com>
2024-04-26 11:54:51 +0300

    İlk Yükleme

---
+++ Models/Ders.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DersModelApp.Models
+{
+    public class Ders
+    {
+        public int Seviye { get; set; }
+        public string Ad { get; set; }
+        public string Ogretmen { get; set; }
+    }
+}
